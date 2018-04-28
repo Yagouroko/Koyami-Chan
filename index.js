9a501de26f29613412f958c1c78f8d2bf2ba729e
Yagouroko <38819051+Yagouroko@users.noreply.github.com>
2018-04-29 01:30:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const client = new Discord.Client();
 
 var prefix = "-";
 
-client.login(process.env.TOKEN);
-
 client.on('message', message => {
     if (message.content === '-createur') {
     	message.reply('Yagouroko!');
