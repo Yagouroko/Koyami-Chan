8e84f9b958afba5f50db747f61c001c18c59fab4
Yagouroko <38819051+Yagouroko@users.noreply.github.com>
2018-04-29 03:43:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var prefix = "-"
 
 client.on('ready', () => {
-    bot.user.setGame("Command: -help");
+	client.user.setGame(" -help https://discord.gg/xTR66sm ")
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === '-ping') {
+    if (message.content === 'ping') {
     	message.channel.send('PONG!');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === '-bing') {
+    if (message.content === 'bing') {
     	message.reply('BONG!');
   	}
 });
 
-client.on('message', message => {
-    if (message.content === '-createur') {
-    	message.reply('Yagouroko!');
-  	}
-});
-
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login("NDM5ODc5NTk0NTc3NzU2MTcw.DcaQ7g.9yK4pgKXI5Ycuu9OCMSakEJc7z8");
