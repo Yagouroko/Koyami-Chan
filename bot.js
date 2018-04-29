47fdb39d53d31d827c61ae5233e14e20472fa9aa
Yagouroko <38819051+Yagouroko@users.noreply.github.com>
2018-04-29 03:04:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,44 +3,9 @@ const client = new Discord.Client();
 
 var prefix = ("-")
 
-bot.on('message', message => {
-    if (message.content === prefix + "help"){
-        message.channel.sendMessage("```Liste des commandes: \n !burgerbot \n !fabriquant \n !burgermusic```");
-    }
-
-    if (message.content === prefix + "burgerbot"){
-        message.channel.sendMessage("```Création du bot le _11/12/2017_ à _13h57_```");
-        console.log("Commande effectué");
-    }
-
-    if (message.content === prefix + "fabriquant"){
-        message.channel.sendMessage("```Ce bot a été crée par MhBurgerKing```");
-        console.log("Commande effectué");
-    }
-    
-    if (message.content === prefix + "burgermusic play"){
-        message.channel.sendMessage("```BurgerMusic bientôt disponible !```");
-        console.log("Commande effectué");
-    }
-    
-    if (message.content === prefix + "burgermusic stop"){
-        message.channel.sendMessage("```BurgerMusic bientôt disponible !```");
-        console.log("Commande effectué");
-    }
-    
-    if (message.content === prefix + "burgermusic next"){
-        message.channel.sendMessage("```BurgerMusic bientôt disponible !```");
-        console.log("Commande effectué");
-    }
-    
-    if (message.content === prefix + "burgermusic"){
-        message.channel.sendMessage("```BurgerMusic:\n !burgermusic play\n !burgermusic next\n !burgermusic stop```");
-        console.log("Commande effectué");
-    }
-});
-client.on('ready', () => {
-    console.log('I am ready!');
-    client.user.setGame("Test")
+bot.on('ready', function() {
+    bot.user.setGame("Command: !help");
+    console.log("Connected");
 });
 
 client.on('message', message => {
