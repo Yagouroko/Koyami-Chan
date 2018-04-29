03a1180e95106ed93f4918b4e4cfdc60d1ed3c07
Yagouroko <38819051+Yagouroko@users.noreply.github.com>
2018-04-29 02:17:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "-";
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setGame("Coucou");
+});
+
+client.on('message', message => {
+    if (message.content === '-ping') {
+    	message.channel.send('PONG!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '-bing') {
+    	message.reply('BONG!');
+  	}
 });
 
 client.on('message', message => {
