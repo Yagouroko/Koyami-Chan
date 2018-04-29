3772986bfc9cda63f920960c0f2f701e0e7a0c10
Yagouroko <38819051+Yagouroko@users.noreply.github.com>
2018-04-29 03:48:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
-var prefix = "-"
 
 client.on('ready', () => {
-client.user.setGame(" -help https://discord.gg/xTR66sm ")
-console.log('I am ready!');
+	client.user.setGame(" -help https://discord.gg/xTR66sm ")
+    console.log('I am ready!');
 });
+
+var prefix = "-";
+
+client.login(process.env.TOKEN);
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
+    if (message.content === '-createur') {
+    	message.reply('Yagouroko!');
   	}
 });
-
-client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
-client.login("NDM5ODc5NTk0NTc3NzU2MTcw.DcaQ7g.9yK4pgKXI5Ycuu9OCMSakEJc7z8");
